a2d28e2385557ad248fa74d063e4a5c0cda40c03
Robert Manzke <ctag-fh-kiel@users.noreply.github.com>
2023-11-17 18:36:16 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 git config --global --add safe.directory /github/workspace
 
-build_output=$(idf.py build)
+build_output=$(idf.py update-dependencies build)
 build_output="${build_output//'%'/'%25'}"
 build_output="${build_output//$'\n'/'%0A'}"
 build_output="${build_output//$'\r'/'%0D'}"
